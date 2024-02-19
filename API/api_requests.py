c6cc350bfb943318e96de50f0ad7eb28cdd7656a
Shashaev <shashaevkirill@gmail.com>
2024-02-19 21:18:51 +0300

    3

---
+++ API/api_requests.py
@@ -0,0 +1,2 @@
+base_api = 'https://api.hh.ru/'
+get_areas_api = base_api + '/areas'
--- API/main.py
+++ API/main.py
@@ -1,32 +1,40 @@
 import requests
+from api_requests import *
 
 
-def get_vacancies(keyword):
-    url = "https://api.hh.ru/vacancies"
-    params = {
-        "text": keyword,
-        "area": 1,  # Specify the desired area ID (1 is Moscow)
-        "per_page": 1,  # Number of vacancies per page
-    }
-    headers = {
-        "User-Agent": "Your User Agent",  # Replace with your User-Agent header
-    }
+def get_areas_tree() -> any:
+    req = requests.get(get_areas_api)
+    data = req.json()
+    req.close()
 
-    response = requests.get(url, params=params, headers=headers)
-
-    if response.status_code == 200:
-        data = response.json()
-        vacancies = data.get("items", [])
-        for vacancy in vacancies:
-            # Extract relevant information from the vacancy object
-            vacancy_id = vacancy.get("id")
-            vacancy_title = vacancy.get("name")
-            vacancy_url = vacancy.get("alternate_url")
-            company_name = vacancy.get("employer", {}).get("name")
-            result = f"ID: {vacancy_id}\nTitle: {vacancy_title}\nCompany: {company_name}\nURL: {vacancy_url}\n"
-            return result
-    else:
-        print(f"Request failed with status code: {response.status_code}")
+    return data
 
 
-print(get_vacancies("python"))
+def pars_areas(areas_tree: any, areas: dict) -> None:
+    for area in areas_tree:
+        areas[area['name']] = area['id']
+
+        if area.get('areas'):
+            pars_areas(area['areas'], areas)
+
+
+def get_areas(is_all: bool = False):
+    areas_tree = get_areas_tree()
+    areas = {}
+    pars_areas(areas_tree, areas)
+
+    if is_all:
+        for name in list(areas):
+            areas[areas[name]] = name
+
+    return areas
+
+
+def main():
+    data = get_areas(True)
+    print(data)
+    print(data.get('Нижний Новгород'))
+
+
+if __name__ == '__main__':
+    main()
