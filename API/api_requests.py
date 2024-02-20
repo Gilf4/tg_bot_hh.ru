e5355b1f0d1af51a2fb7ece9e72e4153f3d6e0a8
Egor Smirnov <esmirnov1234@gmail.com>
2024-02-20 18:34:22 +0300

    added the ability to search for a vacancies by user request in telegram

--- API/api_requests.py
+++ API/api_requests.py
@@ -1,2 +1,3 @@
 base_api = 'https://api.hh.ru/'
 get_areas_api = base_api + '/areas'
+get_vacancies_api = base_api + '/vacancies'
---
+++ API/get_vacancies.py
@@ -0,0 +1,19 @@
+import requests
+
+URL_VACANCIES = 'https://api.hh.ru/vacancies'
+
+
+def get_vacancies(language, page=0, per_page=10):
+    params = {
+        'text': f'NAME:{language}',
+        'per_page': per_page,
+        'page': page,
+        # сортировка вакансий по дате добавления компании этой вакансии,
+        # навреное надо будет выпилить потому что какие-то странные ваканции выдает
+        #'order_by': 'publication_time'
+    }
+    req = requests.get(URL_VACANCIES, params=params)
+    if req.status_code == 200:
+        return req.json()
+    else:
+        return None
