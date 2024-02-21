16569f077cbcb9dd86788eb1901de90fc85bf037
Shashaev <shashaevkirill@gmail.com>
2024-02-21 20:58:52 +0300

    7

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,4 @@
+from API.handler_api import get_areas_json, get_vacancies
 
 
 def json_to_dict(areas_tree: any, areas: dict) -> None:
@@ -24,3 +25,40 @@ def get_areas(is_smarted: bool = False):
             areas[areas[name]] = name
 
     return areas
+
+
+def format_salary(salary_dict):
+    if salary_dict:
+        salary_from = salary_dict.get('from', 'Не указана')
+        salary_to = salary_dict.get('to', 'Не указана')
+        salary_currency = salary_dict.get('currency', '')
+        # Преобразование None в "Не указана"
+        if salary_from is None:
+            salary_from = "Не указана"
+        if salary_to is None:
+            salary_to = "Не указана"
+        if salary_to == "Не указана":
+            return f"{salary_from} {salary_currency}"
+        else:
+            return f"{salary_from} - {salary_to} {salary_currency}"
+    else:
+        return "Зарплата не указана"
+
+
+def format_vacancies(text):
+    vacancies = get_vacancies(text)
+    if vacancies:
+        vacancies_text = ''
+        for item in vacancies.get('items', []):
+            salary_text = format_salary(item.get('salary'))
+            vacancies_text += f"Название вакансии: {item['name']}\n"
+            vacancies_text += f"Зарплата: {salary_text}\n"
+            vacancies_text += f"Компания: {item['employer']['name']}\n"
+            vacancies_text += f"Город: {item['area']['name']}\n"
+            vacancies_text += f"URL вакансии: {item['alternate_url']}\n\n"
+        if vacancies_text:
+            return vacancies_text
+        else:
+            return "По вашему запросу ничего не найдено."
+    else:
+        return "Ошибка при запросе"
