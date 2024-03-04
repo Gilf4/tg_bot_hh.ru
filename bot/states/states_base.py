700e43b012d3ea053460453055ceb032cb135e0b
Shashaev <shashaevkirill@gmail.com>
2024-03-04 20:41:11 +0300

    create new states ...

--- bot/states/states_base.py
+++ bot/states/states_base.py
@@ -3,4 +3,8 @@ from aiogram.fsm.state import StatesGroup, State
 
 class StepsBase(StatesGroup):
     GET_QUERY = State()
+    GET_FILTER = State()
+    GET_SORTS = State()
+    GET_AREA = State()
+    GET_SEARCH_FIELD = State()
     BASE_WORK = State()
